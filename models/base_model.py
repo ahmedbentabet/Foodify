48a2dd8502ddf2c8fced99f90551f678909c9a0d
Tariq5mo <tariqomer756@gmail.com>
2024-12-18 20:50:27 +0200

    Add base file

--- models/base_model.py
+++ models/base_model.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+"""This module contains the base model for the application."""
 import uuid
 from datetime import datetime
 from sqlalchemy import Column, String, TIMESTAMP
@@ -6,14 +8,15 @@ from sqlalchemy.ext.declarative import declarative_base
 # SQLAlchemy Base
 Base = declarative_base()
 
+
 class BaseModel(Base):
     """
     Base model that contains common fields for all tables.
     """
     __abstract__ = True  # Ensures no table is created for BaseModel
 
-    id = Column(String(36), primary_key=True, unique=True, nullable=False, 
+    id = Column(String(36), primary_key=True, unique=True, nullable=False,
                 default=lambda: str(uuid.uuid4()))
     created_at = Column(TIMESTAMP, default=datetime.utcnow, nullable=False)
-    updated_at = Column(TIMESTAMP, default=datetime.utcnow, 
+    updated_at = Column(TIMESTAMP, default=datetime.utcnow,
                         onupdate=datetime.utcnow, nullable=False)
