3ffde539db6ea68959a15cd9d29ba3e586f83611
Ahmed Bentabet <61090548+ahmedbentabet@users.noreply.github.com>
2024-12-28 11:39:45 +0000

    Update the docstring

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module instantiates an object of classes FileStorage and DBStorage"""
+"""This module instantiates an object of the DBStorage classe!"""
 
 from models.engine.db_storage import DBStorage
 storage = DBStorage() 
