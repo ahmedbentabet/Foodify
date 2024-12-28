9f0ebe8f13e2b6d6963a20e9f4a5911c6b859183
Ahmed Bentabet <61090548+ahmedbentabet@users.noreply.github.com>
2024-12-28 11:35:27 +0000

    Initialize DBStorage and enable storage reload in models module

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,7 @@
-from .user import User
-from .restaurant import Restaurant
-from .menu_item import MenuItem
-from .review import Review
-from .order import Order
+#!/usr/bin/python3
+"""This module instantiates an object of classes FileStorage and DBStorage"""
+
+from models.engine.db_storage import DBStorage
+storage = DBStorage() 
+
+storage.reload()
