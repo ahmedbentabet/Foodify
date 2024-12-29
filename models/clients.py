9e398281ff5ee22dc599e0e71f3475272d6c580a
Ahmed Bentabet <61090548+ahmedbentabet@users.noreply.github.com>
2024-12-29 08:34:13 +0000

    Refactor Client class

---
+++ models/clients.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Client model module"""
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from base_model import BaseModel
+
+
+
+class Client(BaseModel):
+    """Client model"""
+    
+    __tablename__ = 'clients'
+
+    username = Column(String(50), nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    password_hash = Column(String(255), nullable=False)
+
+    def __init__(self, *args, **kwargs):
+        """Initialize client"""
+        super().__init__(*args, **kwargs)
+
+# Test
+
+# my_client = Client(username="Ahmed", id="Ahmed", email="ahmed@example.com",
+#                 password_hash="hashedpassword")
+    
+
+# print("Created Model:", my_client)
+
+# import models
+# storage.__session.add(my_client)
+# print("Model Saved:", my_client)
