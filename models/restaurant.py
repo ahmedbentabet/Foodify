4a8cee28ac1ef55fb3328b58cb4f7881340bd8d3
Ahmed Bentabet <61090548+ahmedbentabet@users.noreply.github.com>
2024-12-31 12:26:54 +0000

    Update files

--- models/restaurant.py
+++ models/restaurant.py
@@ -1,17 +1,1 @@
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-from base_model import BaseModel
 
-class Restaurant(BaseModel):
-    """
-    Restaurant model to store restaurant information.
-    """
-    __tablename__ = 'restaurant'
-
-    name = Column(String, nullable=False)
-    location = Column(String, nullable=False)
-    owner_id = Column(String(36), ForeignKey('user.id'), nullable=False)
-    cuisine_type = Column(String, nullable=True)
-
-    # Relationship to the User who owns the restaurant
-    owner = relationship('User', backref='restaurants')
