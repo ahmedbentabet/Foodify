394807475a5a0946fc564b298eb3e4ee35f136ac
Ahmed Bentabet <61090548+ahmedbentabet@users.noreply.github.com>
2025-01-04 13:52:59 +0000

    Update the init file

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module instantiates an object of the DBStorage class"""
 
-from models.engine.db_storage import DBStorage
+from engine.db_storage import DBStorage
 storage = DBStorage() 
 
 storage.reload()
