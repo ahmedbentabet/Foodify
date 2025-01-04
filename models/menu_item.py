a21104e29f93fc7a5d0dd7f57a8444032de94b4f
Ahmed Bentabet <61090548+ahmedbentabet@users.noreply.github.com>
2025-01-04 13:55:26 +0000

    Update the MenuItem class

--- models/menu_item.py
+++ models/menu_item.py
@@ -1,19 +1,19 @@
 from sqlalchemy import Column, Integer, String, DECIMAL, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
-from models.base_model import BaseModel
+from base_model import BaseModel, Base
 
-class MenuItem(BaseModel):
+class MenuItem(BaseModel, Base):
     """MenuItem model"""
     
     __tablename__ = 'menu_items'
 
-    restaurant_id = Column(Integer, ForeignKey('restaurants.id'), nullable=False)
+    restaurant_id = Column(String(60), ForeignKey('restaurants.id'), nullable=False)
     name = Column(String(100), nullable=False)
     price = Column(DECIMAL(10, 2), nullable=False)
     is_available = Column(Boolean, default=True)
 
     restaurant = relationship("Restaurant", back_populates="menu_items")
-    order_items = relationship("OrderItem", back_populates="menu_item")
+    order_items = relationship("OrderItem", back_populates="menu_item", cascade="all, delete-orphan")
 
     def __init__(self, *args, **kwargs):
         """Initialize menu item"""
