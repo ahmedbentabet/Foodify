ed97c19be60eb24ed10f1ee658cf6673071b20b5
Ahmed Bentabet <61090548+ahmedbentabet@users.noreply.github.com>
2025-01-09 10:57:59 +0000

    Add total_price column and improve code formatting and string consistency

--- models/order.py
+++ models/order.py
@@ -12,6 +12,7 @@ class Order(BaseModel, Base):
 
     client_id = Column(String(60), ForeignKey('clients.id', ondelete="CASCADE"),nullable=False)
     status = Column(String(20), nullable=False, default="active")  # active, completed, cancelled
+    total_price = Column(DECIMAL(10, 2), nullable=False)  # Added total_price field
     order_date = Column(DateTime, default=datetime.utcnow)
 
     client = relationship("Client", back_populates="orders")
